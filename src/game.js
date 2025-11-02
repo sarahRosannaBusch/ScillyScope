112ae198ef5acbf6bf2dea69855a45f24f0da723
Sarah Rosanna Busch <annarose0113@gmail.com>
2025-11-02 11:21:22 -0800

    files created

---
+++ src/game.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * @file    game.js
+ * @brief   ScillyScope scripts
+ * @authors Sarah Busch
+ * @version 0
+ * @date    2 Nov 2025
+ */
+
+var game = (function() {
+    var that = {}; // public objects
+
+    that.init = function() {
+        console.log('page loaded');
+    }
+
+    return that;
+}());
